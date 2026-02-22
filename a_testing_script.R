33c359df7c73291e3872f0085411161795e1a453
MeganMarieMartinez <mmartinez30@luc.edu>
2026-02-22 15:05:27 -0600

    added a new line

--- a_testing_script.R
+++ a_testing_script.R
@@ -1,10 +1,17 @@
 data(iris)
 View(iris)
-
+library(corrplot)
 # doing some analysis on the iris dataset
 
 summary(iris)
+scale(iris[1:4])
 plot(iris, col = 'magenta3')
 
 # performing pca on the dataset (not going to split into testing or training)
 
+pc <- prcomp(iris[1:4])
+summary(pc)
+
+biplot(pc)
+
+colnames(iris)
